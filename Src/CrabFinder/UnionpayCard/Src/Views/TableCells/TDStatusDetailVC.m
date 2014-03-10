7575bf55cb8ba28eabb97153f7a1cf4d15b0e7b0
Daniel Dong <ydong@gagein.com>
2014-03-10 13:57:04 +0800

    status detail vc

--- Src/CrabFinder/UnionpayCard/Src/Views/TableCells/TDStatusDetailVC.m
+++ Src/CrabFinder/UnionpayCard/Src/Views/TableCells/TDStatusDetailVC.m
@@ -8,7 +8,14 @@
 
 #import "TDStatusDetailVC.h"
 
-@interface TDStatusDetailVC ()
+@interface TDStatusDetailVC () {
+    UIImageView     *_ivPhoto;
+    UILabel         *_lblName;
+    UILabel         *_lblTitle;
+    
+    UIView          *_pictureContainer;
+    NSMutableArray  *_pictureViews;
+}
 
 @end
 
@@ -18,6 +25,70 @@
 {
     [super viewDidLoad];
     self.navigationItem.title = @"Status Detail";
+    _pictureViews = [NSMutableArray array];
+    
+    [self setupViews];
+    [self setupConstraints];
 }
+
+-(void)setupViews {
+    _ivPhoto = [UIImageView new];
+    _ivPhoto.image = [TDImageLibrary sharedInstance].avatar;
+    [self.view addSubview:_ivPhoto];
+    
+    _lblName = [UILabel new];
+    _lblName.text = @"Barack Obama";
+    [self.view addSubview:_lblName];
+    
+    _lblTitle = [UILabel new];
+    _lblTitle.text = _statusUpdate.title;
+    _lblTitle.numberOfLines = 0;
+    _lblTitle.font = [TDFontLibrary sharedInstance].fontNormal;
+    [self.view addSubview:_lblTitle];
+    
+    _pictureContainer = [UIView new];
+    //_pictureContainer.backgroundColor = [FDColor sharedInstance].caribbeanGreen;
+    [self.view addSubview:_pictureContainer];
+    
+    for (int i = 0; i < 4; i++) {
+        UIImageView *iv = [UIImageView new];
+        iv.image = [TDImageLibrary sharedInstance].farmersMarket;
+        [_pictureContainer addSubview:iv];
+        [_pictureViews addObject:iv];
+    }
+}
+
+-(void)setupConstraints {
+    [_ivPhoto alignTop:@"20" leading:@"20" toView:self.view];
+    [_ivPhoto constrainWidth:@"50" height:@"50"];
+    
+    [_lblName constrainLeadingSpaceToView:_ivPhoto predicate:@"20"];
+    [_lblName alignCenterYWithView:_ivPhoto predicate:nil];
+    
+    [_lblTitle alignLeading:@"20" trailing:@"-20" toView:self.view];
+    [_lblTitle constrainTopSpaceToView:_ivPhoto predicate:@"20"];
+    
+    [_pictureContainer constrainWidth:@"200" height:@"200"];
+    [_pictureContainer alignCenterXWithView:self.view predicate:nil];
+    [_pictureContainer constrainTopSpaceToView:_lblTitle predicate:@"20"];
+    
+    UIImageView  *iv;
+    for (iv in _pictureViews) {
+        [iv constrainWidth:@"95" height:@"95"];
+    }
+    
+    iv = _pictureViews[0];
+    [iv alignTop:@"0" leading:@"0" toView:_pictureContainer];
+    iv = _pictureViews[1];
+    [iv alignTopEdgeWithView:_pictureContainer predicate:nil];
+    [iv alignTrailingEdgeWithView:_pictureContainer predicate:nil];
+    iv = _pictureViews[2];
+    [iv alignBottomEdgeWithView:_pictureContainer predicate:nil];
+    [iv alignLeadingEdgeWithView:_pictureContainer predicate:nil];
+    iv = _pictureViews[3];
+    [iv alignTrailingEdgeWithView:_pictureContainer predicate:nil];
+    [iv alignBottomEdgeWithView:_pictureContainer predicate:nil];
+}
+
 
 @end
